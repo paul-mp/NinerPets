2608f2c4265b9dceb50bca3c42642a722a7f1fe0
pistanij <pistanij@gmail.com>
2024-10-19 15:57:59 -0400

    Added functionality to the view details button to show a pop-up of information

--- niner-pets/src/Pages/Billing.js
+++ niner-pets/src/Pages/Billing.js
@@ -1,8 +1,46 @@
-import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import { Box, Button, Grid, Paper, Typography, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import BookIcon from '@mui/icons-material/Book';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Billing = () => {
+  const [open, setOpen] = useState(false);
+  const [balanceType, setBalanceType] = useState('');
+
+  const handleClickOpen = (type) => {
+    setBalanceType(type);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setBalanceType('');
+  };
+
+  // Sample balance details
+  const balanceDetails = {
+    Appointment: {
+      amountDue: '$125.00',
+      details: [
+        { date: '2024-10-01', description: 'Vet Visit', amount: '$50.00' },
+        { date: '2024-10-15', description: 'Dental Checkup', amount: '$75.00' },
+      ],
+    },
+    Medication: {
+      amountDue: '$50.00',
+      details: [
+        { name: 'Apoquel 16mg', amount: '$25.00' },
+        { name: 'Galliprant 20mg', amount: '$25.00' },
+      ],
+    },
+    Vaccine: {
+      amountDue: '$40.00',
+      details: [
+        { name: 'Rabies Vaccine', amount: '$15.00' },
+        { name: 'Distemper Vaccine', amount: '$25.00' },
+      ],
+    },
+  };
+
   return (
     <Box sx={{ flexGrow: 1, padding: 2 }}>
       <Box 
@@ -26,165 +64,81 @@ const Billing = () => {
       </Box>
 
       <Grid container spacing={3} sx={{ marginTop: 0 }}> 
-        <Grid item xs={6}>
-          <Paper 
-            elevation={3} 
-            sx={{ 
-              width: '100%', 
-              height: '300px', 
-              margin: '0 auto', 
-              position: 'relative', 
-              padding: 2, 
-              boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.5)', 
-              boxSizing: 'border-box',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
-              Appointment Balance
-            </Typography>
-            <Typography variant="h5" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              Amount Due:
-            </Typography>
-            <Typography 
-              variant="h4" 
+        {['Appointment', 'Medication', 'Vaccine'].map((type) => (
+          <Grid item xs={6} key={type}>
+            <Paper 
+              elevation={3} 
               sx={{ 
-                flexGrow: 1, 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center', 
-                color: 'primary.main' 
+                width: '100%', 
+                height: '300px', 
+                margin: '0 auto', 
+                position: 'relative', 
+                padding: 2, 
+                boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.5)', 
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'column',
               }}
             >
-              $XXX
-            </Typography>
-            <hr style={{ 
-              margin: '20px 0', 
-              border: '0.5px solid black', 
-              width: 'calc(100% + 32px)',
-              position: 'relative',
-              left: '-16px',
-            }} />
-            <Button 
-              variant="contained" 
-              color="primary" 
-              sx={{ alignSelf: 'flex-start', display: 'flex', alignItems: 'center' }}
-            >
-              <BookIcon sx={{ marginRight: 1 }} />
-              <Typography sx={{ color: 'white' }}>
-                View Balance Details
+              <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
+                {type} Balance
               </Typography>
-            </Button>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={6}>
-          <Paper 
-            elevation={3} 
-            sx={{ 
-              width: '100%', 
-              height: '300px', 
-              margin: '0 auto', 
-              position: 'relative', 
-              padding: 2, 
-              boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.5)', 
-              boxSizing: 'border-box',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
-              Medication Balance
-            </Typography>
-            <Typography variant="h5" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              Amount Due:
-            </Typography>
-            <Typography 
-              variant="h4" 
-              sx={{ 
-                flexGrow: 1, 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center', 
-                color: 'primary.main' 
-              }}
-            >
-              $XXX
-            </Typography>
-            <hr style={{ 
-              margin: '20px 0', 
-              border: '0.5px solid black', 
-              width: 'calc(100% + 32px)', 
-              position: 'relative', 
-              left: '-16px',
-            }} />
-            <Button 
-              variant="contained" 
-              color="primary" 
-              sx={{ alignSelf: 'flex-start', display: 'flex', alignItems: 'center' }}
-            >
-              <BookIcon sx={{ marginRight: 1 }} />
-              <Typography sx={{ color: 'white' }}>
-                View Balance Details
+              <Typography variant="h5" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                Amount Due:
               </Typography>
-            </Button>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={6}>
-          <Paper 
-            elevation={3} 
-            sx={{ 
-              width: '100%', 
-              height: '300px', 
-              margin: '0 auto', 
-              position: 'relative', 
-              padding: 2, 
-              boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.5)', 
-              boxSizing: 'border-box',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
-              Vaccine Balance
-            </Typography>
-            <Typography variant="h5" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              Amount Due:
-            </Typography>
-            <Typography 
-              variant="h4" 
-              sx={{ 
-                flexGrow: 1, 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center', 
-                color: 'primary.main' 
-              }}
-            >
-              $XXX
-            </Typography>
-            <hr style={{ 
-              margin: '20px 0', 
-              border: '0.5px solid black', 
-              width: 'calc(100% + 32px)', 
-              position: 'relative', 
-              left: '-16px',
-            }} />
-            <Button 
-              variant="contained" 
-              color="primary" 
-              sx={{ alignSelf: 'flex-start', display: 'flex', alignItems: 'center' }}
-            >
-              <BookIcon sx={{ marginRight: 1 }} />
-              <Typography sx={{ color: 'white' }}>
-                View Balance Details
+              <Typography 
+                variant="h4" 
+                sx={{ 
+                  flexGrow: 1, 
+                  display: 'flex', 
+                  justifyContent: 'center', 
+                  alignItems: 'center', 
+                  color: 'primary.main' 
+                }}
+              >
+                {balanceDetails[type]?.amountDue}
               </Typography>
-            </Button>
-          </Paper>
-        </Grid>
+              <hr style={{ 
+                margin: '20px 0', 
+                border: '0.5px solid black', 
+                width: 'calc(100% + 32px)',
+                position: 'relative',
+                left: '-16px',
+              }} />
+              <Button 
+                variant="contained" 
+                color="primary" 
+                sx={{ alignSelf: 'flex-start', display: 'flex', alignItems: 'center' }}
+                onClick={() => handleClickOpen(type)}
+              >
+                <BookIcon sx={{ marginRight: 1 }} />
+                <Typography sx={{ color: 'white' }}>
+                  View Balance Details
+                </Typography>
+              </Button>
+            </Paper>
+          </Grid>
+        ))}
       </Grid>
+
+      {/* Dialog for Balance Details */}
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+        <DialogTitle>{balanceType} Balance Details</DialogTitle>
+        <DialogContent>
+          {balanceDetails[balanceType]?.details.map((item, index) => (
+            <Typography key={index} variant="body1" sx={{ marginBottom: 1 }}>
+              {balanceType === 'Appointment'
+                ? `${item.date}: ${item.description} - ${item.amount}`
+                : `${item.name} - ${item.amount}`}
+            </Typography>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
